12bbcd49e4a12e8299b252969c12461547ac788b
Marcos642 <marcostadeu642@gmail.com>
2024-03-04 23:04:01 -0300

    FRONT-END TELA DE LOGIN

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 session_start();
 require "vendor/autoload.php";
 define("INCLUDE_PATH_STATIC","http://localhost/REDE_SOCIAL/SocialMedia/Views/Pages/");
+define("INCLUDE_PATH","http://localhost/REDE_SOCIAL/"); #Para criar conta 
+
 $app = new SocialMedia\Aplication;
 $app->run();
