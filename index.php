66022530c8d5b0a1fde07d8cb6deaaed1b8ae3e6
Marcos642 <marcostadeu642@gmail.com>
2024-03-19 10:15:21 -0300

    FRONT-END TELA DE LOGIN 2.0
    Apenas comentários realizados

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 require "vendor/autoload.php";
-define("INCLUDE_PATH_STATIC","http://localhost/REDE_SOCIAL/SocialMedia/Views/Pages/");
-define("INCLUDE_PATH","http://localhost/REDE_SOCIAL/"); #Para criar conta 
+define("INCLUDE_PATH_STATIC","http://localhost/REDE_SOCIAL/SocialMedia/Views/Pages/");// Definir qual diretorio vai incluir os arquivos estáticos
+define("INCLUDE_PATH","http://localhost/REDE_SOCIAL/"); #Para criar conta (PARA REDIRECIONAMENTO COM LINK)
 
 $app = new SocialMedia\Aplication;
 $app->run();
